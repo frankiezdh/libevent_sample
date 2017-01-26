237ced63512e2b874b095c34fdb8aa8652414cd4
frankiezdh <frankiezdh@gmail.com>
2017-01-26 20:55:36 +0800

    精简部分代码

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-#include <iomanip>
 #include <event.h>
 #include <event2/thread.h>
 
@@ -8,24 +7,17 @@
 using namespace std;
 
 void on_time(evutil_socket_t fd, short event, void *data) {
-    (void)fd;
-    (void)event;
+    (void) fd;
+    (void) event;
+    (void) data;
 
-    auto t = time(nullptr);
-    auto tm = *localtime(&t);
+    time_t raw_time = time(NULL);
     cout << "[" << pthread_self() << "]"
-         << "定时事件: " << put_time(&tm, "%Y-%m-%d %H:%M:%S")<< endl;
-
-    struct timeval tv;
-    memset(&tv, 0, sizeof(tv));
-    tv.tv_sec = 2;
-    tv.tv_usec = 0;
-
-    evtimer_add((struct event *)data, &tv);
+         << "定时事件: " << ctime(&raw_time);
 }
 
 void *time_thread(void *arg) {
-    (void)arg;
+    (void) arg;
 
     struct event_base *base = event_base_new();
     if (base == NULL) {
@@ -35,7 +27,7 @@ void *time_thread(void *arg) {
 
     struct event time_event;
     memset(&time_event, 0, sizeof(time_event));
-    evtimer_set(&time_event, on_time, (void *)&time_event);
+    event_set(&time_event, -1, EV_PERSIST, on_time, NULL);
     event_base_set(base, &time_event);
 
     struct timeval tv;
@@ -51,7 +43,8 @@ void *time_thread(void *arg) {
     return NULL;
 }
 
-#define THREAD_MAX_NUM 4
+#define THREAD_MAX_NUM 2
+
 int main() {
     evthread_use_pthreads();
 
@@ -59,11 +52,11 @@ int main() {
     memset(threads, 0, sizeof(threads));
 
     for (int i = 0; i < THREAD_MAX_NUM; ++i) {
-        (void)pthread_create(&threads[i], NULL, time_thread, NULL);
+        (void) pthread_create(&threads[i], NULL, time_thread, NULL);
     }
 
     for (int i = 0; i < THREAD_MAX_NUM; ++i) {
-        (void)pthread_join(threads[i], NULL);
+        (void) pthread_join(threads[i], NULL);
     }
 
     return 0;
